bf7de6eb6e0cceed1eef06c1a47b5e5b07704052
Kok <fkok@sensata.com>
2023-02-09 14:18:20 +0800

    Add project files.

---
+++ STM Mechanization/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Identity;
+
+/*
+int uniqueUsers;
+
+int percentComplete = (int)((double)uniqueUsers / 100 * 100);
+
+var script = $"document.getElementById('progressBar').style.width = '{percentComplete}%';";
+script += $"document.getElementById('progressBar').innerHTML = '{percentComplete}
+*/
+
+
+namespace STM_Mechanization.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
